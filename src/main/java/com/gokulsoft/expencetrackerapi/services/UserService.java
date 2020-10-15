13b65f56c1c2cc10f534fd117f2016ebd82c15bb
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-10-15 23:51:26 +0200

    add exception class

--- src/main/java/com/gokulsoft/expencetrackerapi/services/UserService.java
+++ src/main/java/com/gokulsoft/expencetrackerapi/services/UserService.java
@@ -1,5 +1,11 @@
 package com.gokulsoft.expencetrackerapi.services;
 
-public interface UserService {
+import com.gokulsoft.expencetrackerapi.domain.User;
+import com.gokulsoft.expencetrackerapi.exceptions.EtAuthException;
 
+public interface UserService {
+	User validateUser(String email,String password) throws EtAuthException;
+	
+	User reisterUser(String firstName,String lastName,String email,String password) throws EtAuthException;
+	
 }
