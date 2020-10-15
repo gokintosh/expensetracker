cdc5313271556b787fc67099dcd56cddd8ba9067
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-10-15 14:53:56 +0200

    first commit

---
+++ src/main/java/com/gokulsoft/expencetrackerapi/ExpenceTrackerApiApplication.java
@@ -0,0 +1,14 @@
+package com.gokulsoft.expencetrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenceTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenceTrackerApiApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/gokulsoft/expencetrackerapi/ExpenceTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokulsoft.expencetrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenceTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
